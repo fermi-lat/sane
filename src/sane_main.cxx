3760d9e3905bdcb703dae6ee48f3d2b5ff9d1bb8
burnett <>
2004-04-04 21:46:55 +0000

    for Linux

--- src/sane_main.cxx
+++ src/sane_main.cxx
@@ -2,10 +2,16 @@
 
 
 */
+#include <string>
+#include <sstream>
+
 #ifdef WIN32
 #include <process.h>
+std::string pyname("python ");
+#else
+std::string pyname("python2.3 ");
 #endif
-#include <sstream>
+
 #include "st_app/IApp.h"
 
 class LikeApp : public st_app::IApp {
@@ -15,7 +21,7 @@ class LikeApp : public st_app::IApp {
         std::stringstream cmd;
         std::string app("/python/menu.py");
 
-        cmd << "python " << ::getenv("SANEROOT") << app;
+        cmd << pyname<< ::getenv("SANEROOT") << app;
 
         system(cmd.str().c_str());
     }
