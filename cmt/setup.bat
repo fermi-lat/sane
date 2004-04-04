ba751c2aad2020d8afbe46a690d9a7578ab28ad0
cvs2git <>
2004-04-04 23:01:07 +0000

    This commit was manufactured by cvs2svn to create tag 'v1'.
    Sprout from master 2004-04-04 23:01:06 UTC burnett 'about to tag version for ST latest'
    Cherrypick from THB 2004-04-02 14:06:04 UTC burnett 'initial import for testing':
        .cvsignore
        python/.cvsignore
    Delete:
        cmt/NMake
        cmt/cleanup.bat
        cmt/like.bat
        cmt/setup.bat

--- cmt/setup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if .%1==. (set tag=VC8debug ) else set tag=%1
-set tempfile=%HOME%\tmpsetup.bat
-d:\ground\CMT\v1r14p20031120\VisualC\cmt.exe -quiet -bat -pack=like -version=v1 setup -tag=%tag% >"%tempfile%"
-if exist "%tempfile%" call "%tempfile%"
-if exist "%tempfile%" del "%tempfile%"
-set PATH=%LD_LIBRARY_PATH%;%PATH%
