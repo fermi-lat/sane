39627726355320de8b49e483c67308764eecce3e
jchiang <>
2010-06-05 16:20:50 +0000

    add setup files for envvars required by test_sane that SCons does not provide

---
+++ data/setup.csh
@@ -0,0 +1,15 @@
+#
+# $Id$
+#
+setenv DATASUBSELECTORROOT ${INST_DIR}/dataSubselector
+setenv LIKELIHOODROOT ${INST_DIR}/Likelihood
+setenv OBSERVATIONSIMROOT ${INST_DIR}/observationSim
+setenv MAP_TOOLSROOT ${INST_DIR}/map_tools
+setenv EVTBINROOT ${INST_DIR}/evtbin
+setenv RSPGENROOT ${INST_DIR}/rspgen
+setenv PULSEPHASEROOT ${INST_DIR}/pulsePhase
+setenv PERIODSEARCHROOT ${INST_DIR}/periodSearch
+setenv ST_APPROOT ${INST_DIR}/st_app
+setenv PYLIKELIHOODROOT ${INST_DIR}/pyLikelihood
+setenv FACILITIESROOT ${INST_DIR}/facilities
+setenv SANEROOT ${INST_DIR}/sane
---
+++ data/setup.sh
@@ -0,0 +1,15 @@
+#
+# $Id$
+#
+export DATASUBSELECTORROOT=${INST_DIR}/dataSubselector
+export LIKELIHOODROOT=${INST_DIR}/Likelihood
+export OBSERVATIONSIMROOT=${INST_DIR}/observationSim
+export MAP_TOOLSROOT=${INST_DIR}/map_tools
+export EVTBINROOT=${INST_DIR}/evtbin
+export RSPGENROOT=${INST_DIR}/rspgen
+export PULSEPHASEROOT=${INST_DIR}/pulsePhase
+export PERIODSEARCHROOT=${INST_DIR}/periodSearch
+export ST_APPROOT=${INST_DIR}/st_app
+export PYLIKELIHOODROOT=${INST_DIR}/pyLikelihood
+export FACILITIESROOT=${INST_DIR}/facilities
+export SANEROOT=${INST_DIR}/sane
