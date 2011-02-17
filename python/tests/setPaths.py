8b8c93c8a05436f1528cff8dff19d4bf2e235072
jchiang <>
2011-02-17 04:57:21 +0000

    clean out tests of useless code

--- python/tests/setPaths.py
+++ python/tests/setPaths.py
@@ -53,21 +53,17 @@ def setup_env_vars():
         os.environ['DATASUBSELECTORROOT'] = inst_dir('dataSubselector')
         os.environ['LIKELIHOODROOT'] = inst_dir('Likelihood')
         os.environ['OBSERVATIONSIMROOT'] = inst_dir('observationSim')
-        os.environ['MAP_TOOLSROOT'] = inst_dir('map_tools')
         os.environ['EVTBINROOT'] = inst_dir('evtbin')
         os.environ['RSPGENROOT'] = inst_dir('rspgen')
-        os.environ['PULSEPHASEROOT'] = inst_dir('pulsePhase')
-        os.environ['PERIODSEARCHROOT'] = inst_dir('periodSearch')
         os.environ['ST_APPROOT'] = inst_dir('st_app')
         os.environ['PYLIKELIHOODROOT'] = inst_dir('pyLikelihood')
         os.environ['FACILITIESROOT'] = inst_dir('facilities')
         os.environ['SANEROOT'] = inst_dir('sane')
         os.environ['GENERICSOURCESROOT'] = inst_dir('celestialSources/genericSources')
-        os.environ['PULSARDBROOT'] = inst_dir('pulsarDb')
 
 def copy_par_files():
-    packages = ['observationSim', 'map_tools', 'dataSubselector',
-                'Likelihood', 'evtbin', 'rspgen', 'pulsePhase', 'periodSearch']
+    packages = ['observationSim', 'dataSubselector',
+                'Likelihood', 'evtbin', 'rspgen']
     for package in packages:
         pfile_path = os.path.join(os.environ[package.upper()+'ROOT'], 'pfiles')
         pfiles = os.listdir(pfile_path)
